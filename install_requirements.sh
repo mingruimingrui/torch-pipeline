c5312c214c95ffab5a72502db39d16c4d1bcc084
mingruimingrui <mingruimingrui@hotmail.com>
2018-08-15 14:54:00 +0800

    install now uninstalls first

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,5 +1,7 @@
 # Install latest stable of torch-collections
+pip uninstall -y torch-collections
 pip install https://github.com/mingruimingrui/torch-collections/archive/stable-latest.zip
 
 # Install latest stable of torch-datasets
+pip uninstall -y torch-datasets
 pip install https://github.com/mingruimingrui/torch-datasets/archive/stable-latest.zip
