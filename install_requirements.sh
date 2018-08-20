2de326742608f272c65a44b1e7fac82d8f20f532
Wang Ming Rui <mingrui.wang@mingruiwang-mac.local>
2018-08-20 14:28:38 +0800

    changed torch-datasets version to 0.2b

--- install_requirements.sh
+++ install_requirements.sh
@@ -4,4 +4,4 @@ pip install https://github.com/mingruimingrui/torch-collections/archive/0.3b.zip
 
 # Install latest stable of torch-datasets
 pip uninstall -y torch-datasets
-pip install https://github.com/mingruimingrui/torch-datasets/archive/stable-latest.zip
+pip install https://github.com/mingruimingrui/torch-datasets/archive/0.2b.zip
